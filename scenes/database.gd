22f0f99fc0ab86fe2dcb986ea6eb9e73568efbc5
Jack Darker <JackDarker@freenet.de>
2025-12-28 20:24:38 +0100

    database

---
+++ scenes/database.gd
@@ -0,0 +1,183 @@
+extends Node
+
+var db : SQLite = null
+var db_name = "res://data/test"
+const verbosity_level : int = SQLite.VERBOSE
+
+func _ready():
+	dbinit()
+	createTag({"name":"new","groupid":1})
+
+func dbinit():
+	db = SQLite.new()
+	db.path = db_name
+	db.verbosity_level = verbosity_level
+	# Open the database using the db_name found in the path variable
+	db.open_db()
+	var query 
+	query = "CREATE TABLE IF NOT EXISTS Posts (
+		postID INTEGER PRIMARY KEY AUTOINCREMENT, 
+		boardID INTEGER NOT NULL,
+		replyToID INTEGER,
+		name TEXT NOT NULL,
+		subject TEXT,
+		dateTime TEXT,
+		postText TEXT,
+		fileName TEXT,
+		posterID TEXT NOT NULL,
+		fileExt TEXT
+		)"
+	db.query(query)
+	query = "CREATE TABLE IF NOT EXISTS Boards (
+		boardID INTEGER PRIMARY KEY AUTOINCREMENT, 
+		boardName TEXT NOT NULL
+		)"
+	db.query(query)
+	query = "CREATE TABLE IF NOT EXISTS PostTags (
+		postID  INTEGER NOT NULL, 
+		tagID  INTEGER NOT NULL, 
+		PRIMARY KEY (postID,tagID) ON CONFLICT IGNORE
+		)"
+	db.query(query)
+	query = "CREATE TABLE IF NOT EXISTS Tags (
+		ID	INTEGER PRIMARY KEY AUTOINCREMENT,
+		GroupID	INTEGER,
+		Name	TEXT			
+		)"
+	db.query(query)
+	query = "CREATE TABLE IF NOT EXISTS TagGroups (
+		ID	INTEGER PRIMARY KEY AUTOINCREMENT,
+		Name	TEXT,
+		Color	TEXT,
+		FGColor	TEXT,
+		Shape	TEXT			
+		)"
+	db.query(query)
+	query="SELECT COUNT(*) AS CNTREC FROM pragma_table_info('TagGroups') WHERE name='FGColor'"
+	db.query(query)
+	if (db.query_result[0]["CNTREC"]<=0):
+		query = "ALTER TABLE TagGroups ADD FGColor TEXT"
+		db.query(query)
+		query = "ALTER TABLE TagGroups ADD Shape TEXT"
+		db.query(query)
+		
+	_test1()
+
+func _test1():
+	var results
+	var postid
+	var tagid
+	createTagGroup({"name":"aqua","color":"aqua","fgcolor":"white","shape":""});
+	createTagGroup({"name":"gray","color":"gray","fgcolor":"white","shape":""});
+	results = findTagGroups()
+	
+	tagid=createTag({"name":"new","groupid":1})
+	createTag({"name":"old","groupid":2})
+	
+	postid=createPost({"filename":"c:/temp/new","name":"new file"})
+	assignTagToPost(postid,tagid)
+	results=findPost("c:/temp/new")
+	results=findPostByTag("new")
+	
+func createTagGroup(group:Dictionary)->int:
+	if(!group.has("newname")):
+		group["newname"]=group.name
+	var rowID:int=-1
+	var query	
+	var selected_array : Array = db.select_rows("TagGroups", "name='"+group.name+"'", ["ID"])
+	if(selected_array.size()>0):
+		query="Update TagGroups SET name='"+group.newname+"',Color='"+group.color+"',FGColor='"+group.fgcolor+"',Shape='"+group.shape+"' where (name='"+group.name+"')"
+		db.query(query)
+		rowID=selected_array[0]["ID"]
+	else:
+		query="Insert Into TagGroups (name,Color,FGColor,Shape) VALUES('"+group.newname+"','"+group.color+"','"+group.fgcolor+"','"+group.shape+"')"
+		db.query(query)
+		rowID = db.last_insert_rowid
+	return(rowID)
+
+func findTagGroups()->Array:
+	var results : Array = db.select_rows("TagGroups", "", ["ID","Name","Color","FGColor","Shape"])
+	return(results);
+
+func createTag(tag)->int:
+	if(!tag.has("groupid")):
+		tag["groupid"] = 1
+	if(!tag.has("newname")):
+		tag["newname"] = tag.name
+	var query
+	var rowID=-1
+	var selected_array : Array = db.select_rows("Tags", "name='"+tag.name+"'", ["ID"])
+	if(selected_array.size()>0):
+		query= "Update Tags SET name='"+tag.newname+"', groupID='"+tag.groupid+"' where (name='"+tag.name+"')"
+		db.query(query)
+		rowID=selected_array[0]["ID"]
+	else:
+		query="Insert Into Tags (name,GroupID) VALUES('"+tag.newname+"',"+str(tag.groupid)+")"
+		db.query(query)
+		rowID = db.last_insert_rowid
+	return(rowID);
+	
+func deleteTag(tag):
+	#todo only delete if no one uses it, maybe mark for deletion and hide ?
+	db.query("Delete from Tags where (name='"+tag.name+"')")
+
+func findTags()->Array:
+	var query = "SELECT Tags.ID,Tags.Name, TagGroups.ID as GroupID, TagGroups.Color, TagGroups.FGColor, TagGroups.Shape FROM Tags left join TagGroups on Tags.GroupID=TagGroups.ID"
+	db.query(query)
+	return(db.query_result)
+	
+func findPostTags(postId)->Array:
+	var query = "SELECT Tags.ID,Tags.Name, TagGroups.ID as GroupID, TagGroups.Color, TagGroups.FGColor, TagGroups.Shape FROM Tags 
+			inner join PostTags on Tags.ID=PostTags.tagId 
+			left join TagGroups on Tags.GroupID=TagGroups.ID 
+			WHERE PostTags.postID="+str(postId)
+	db.query(query)
+	return(db.query_result)
+
+func createPost(post)->int:
+	post["userId"]="unknown"		#todo
+	post["boardID"]=0
+	var rowID=-1;
+	var selected_array : Array = db.select_rows("Posts", "name='"+post.name+"'", ["ID"])
+	var query
+	if(selected_array.size()>0):
+		query= "Update Posts Set boardId="+post.boardID+",posterID="+post.userId+", fileName='"+post.fileName+"', name='"+post.name+"' where (name='"+post.name+"')"
+		db.query(query)
+		rowID=selected_array[0]["ID"]
+	else:
+		query= "Insert Into Posts (boardID,posterId,fileName,name) VALUES("+post.boardID+","+post.userId+",'"+post.fileName+"','"+post.name+"')"
+		db.query(query)
+		rowID = db.last_insert_rowid
+	return(rowID)
+	
+func getPost(postID)->Array:
+	var results : Array = db.select_rows("Posts", "postID="+str(postID), ["boardID","postID","name","fileName"])
+	return results
+
+func assignTagToPost(postid,tagids):
+	var ids:String = ", ".join(tagids)
+	var query="Delete From PostTags Where postID="+str(postid)+" AND tagId NOT IN("+ids+")"
+	db.query(query)
+	
+	for id in tagids:
+		query = "Insert Into PostTags (postID,tagId) VALUES("+str(postid)+","+str(id)+")"
+		db.query(query)
+
+#relative path+name
+func findPost(search)->Array:
+	var results : Array = db.select_rows("Posts", "fileName="+str(search), ["boardID","postID","name","fileName"])
+	return results
+
+#search doesnt support wildcards !
+func findPostByTag(search)->Array:
+	var query ="select distinct Posts.postID from Posts inner join PostTags on Posts.postID=PostTags.postID inner join Tags on Tags.ID=PostTags.tagID where Tags.name IN("+search+")"
+	db.query(query)
+	return(db.query_result)
+
+func getTagStatistic()->Array:
+	var query = "SELECT Tags.ID,max(Tags.Name) as name,count(Tags.ID) as count,max(TagGroups.Color) as color, max(TagGroups.FGColor) as fgcolor,max(TagGroups.Shape) as shape FROM Tags 
+		inner join PostTags on Tags.ID=PostTags.tagId 
+		left join TagGroups on Tags.GroupID=TagGroups.ID 
+		Group by (Tags.ID) order by count desc "
+	db.query(query)
+	return(db.query_result)
